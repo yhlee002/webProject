2506a041cf7d706a5d55c7483e352a031425bb91
yhlee <xxxoxxo002@gmail.com>
2021-01-31 01:39:18 +0900

    불필요한 파일 제거

--- src/main/java/com/portfolio/demo/project/ProjectApplication.java
+++ src/main/java/com/portfolio/demo/project/ProjectApplication.java
@@ -3,10 +3,7 @@ package com.portfolio.demo.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.*;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"com.portfolio.demo.project.repository"})
