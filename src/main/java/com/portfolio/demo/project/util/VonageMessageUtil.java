1fa52f75b52fa6119f44400f1b2b7119832e6a60
yhlee <xxxoxxo002@gmail.com>
2021-04-14 22:06:03 +0900

    VonageMessageUtil 수정
    계속해서 Unroutable message - reject 오류 발생
    => 수신번호를 0으로 시작해서 발생하는 문제였다.
    지역번호로 시작하게 한 이후 해결됨

--- src/main/java/com/portfolio/demo/project/util/VonageMessageUtil.java
+++ src/main/java/com/portfolio/demo/project/util/VonageMessageUtil.java
@@ -6,11 +6,15 @@ import com.vonage.client.sms.SmsSubmissionResponse;
 import com.vonage.client.sms.messages.TextMessage;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Base64;
 import java.util.ResourceBundle;
 
 @Slf4j
 public class VonageMessageUtil {
+    private static Base64.Encoder encoder = Base64.getEncoder();
+
     private static ResourceBundle resourceBundle = ResourceBundle.getBundle("Res_ko_KR_keys");
+    private final static String APP_ID = resourceBundle.getString("vonageAppId");
     private final static String API_KEY = resourceBundle.getString("vonageMessageKey");
     private final static String API_SECRET = resourceBundle.getString("vonageMessageSecret");
     private final static String sender = resourceBundle.getString("testPhoneNum");
@@ -18,11 +22,12 @@ public class VonageMessageUtil {
     private static VonageClient client = VonageClient.builder().apiKey(API_KEY).apiSecret(API_SECRET).build();
 
     public String sendPinNumber(String certKey, String phone) {
-        TextMessage message = new TextMessage("Vonage APIs",
-                sender,
-                "Movie Info Site 인증번호입니다. - " + certKey);
-
+        String toNumber = "82" + phone.replace("-", "").substring(1); // '000-0000-0000' 형태의 값 변환
+        TextMessage message = new TextMessage(sender, toNumber, "Movie Info Site PIN number : " + certKey);
         SmsSubmissionResponse response = client.getSmsClient().submitMessage(message);
+
+        log.info("to number : "+toNumber);
+        log.info("from number : "+sender);
 
         String result = "";
         if (response.getMessages().get(0).getStatus() == MessageStatus.OK) {
@@ -30,6 +35,7 @@ public class VonageMessageUtil {
             result = "success";
         } else {
             log.info("[Vonage] Message failed with error: " + response.getMessages().get(0).getErrorText());
+            log.info("[Vonage] entire response message: " + response.getMessages());
             result = "fail";
         }
         return result;
