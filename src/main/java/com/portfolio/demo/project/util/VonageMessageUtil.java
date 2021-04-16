fa3bcb1db28b16c326aee60d8068b773c14794f2
yhlee <xxxoxxo002@gmail.com>
2021-04-16 12:43:04 +0900

    VonageMessageUtil 불필요한 로그 제거
    * 이미지 파일 서버 저장 기능 재확인 필요

--- src/main/java/com/portfolio/demo/project/util/VonageMessageUtil.java
+++ src/main/java/com/portfolio/demo/project/util/VonageMessageUtil.java
@@ -14,15 +14,13 @@ public class VonageMessageUtil {
     private static Base64.Encoder encoder = Base64.getEncoder();
 
     private static ResourceBundle resourceBundle = ResourceBundle.getBundle("Res_ko_KR_keys");
-    private final static String APP_ID = resourceBundle.getString("vonageAppId");
     private final static String API_KEY = resourceBundle.getString("vonageMessageKey");
     private final static String API_SECRET = resourceBundle.getString("vonageMessageSecret");
     private final static String sender = resourceBundle.getString("testPhoneNum");
-
     private static VonageClient client = VonageClient.builder().apiKey(API_KEY).apiSecret(API_SECRET).build();
 
     public String sendPinNumber(String certKey, String phone) {
-        String toNumber = "82" + phone.replace("-", "").substring(1); // '000-0000-0000' 형태의 값 변환
+        String toNumber = "82" + phone.replace("-", "").substring(1); // '8210-0000-0000' 형태의 값 변환
         TextMessage message = new TextMessage(sender, toNumber, "Movie Info Site PIN number : " + certKey);
         SmsSubmissionResponse response = client.getSmsClient().submitMessage(message);
 
